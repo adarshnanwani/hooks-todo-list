e8c2c34e9f4f0cd9d060956e2b521d8e86076634
Adarsh Nanwani <adarsh.nanwani@gmail.com>
2020-02-14 11:59:59 +0530

    Local storage support.

--- src/TodoApp.js
+++ src/TodoApp.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Typography from "@material-ui/core/Typography";
 import Paper from "@material-ui/core/Paper";
 import AppBar from "@material-ui/core/AppBar";
@@ -9,12 +9,19 @@ import TodoList from "./TodoList";
 import TodoForm from "./TodoForm";
 
 const TodoApp = () => {
-  const initialTodos = [
-    { id: 1, task: "Feed the cat", completed: false },
-    { id: 2, task: "Grow beard", completed: true },
-    { id: 3, task: "Wash the car", completed: false }
-  ];
+  const initialTodos = JSON.parse(window.localStorage.getItem("todos")) || [];
+
+  // const initialTodos = [
+  //   { id: 1, task: "Feed the cat", completed: false },
+  //   { id: 2, task: "Grow beard", completed: true },
+  //   { id: 3, task: "Wash the car", completed: false }
+  // ];
   const [todos, setTodos] = useState(initialTodos);
+
+  useEffect(() => {
+    window.localStorage.setItem("todos", JSON.stringify(todos));
+  }, [todos]);
+
   const addTodo = newTodoText => {
     setTodos([...todos, { id: uuid(), task: newTodoText, completed: false }]);
   };
